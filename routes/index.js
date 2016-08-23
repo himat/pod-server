3dd94df2ca8de3dfdc5b16cf7d39e7666edcd2f6
Hima T <himat15@yahoo.com>
2016-08-22 21:58:09 -0400

    Added csv to json conversion

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var csvjson = require('csvjson');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/genjson', function(req, res, next) {
+  console.log('about to convert to json');
+  
+  var data = fs.readFileSync('telemetry_sample.csv', {encoding: 'utf8'});
+  var convertOptions = { delimeter : ',' };
+  
+  var jsonobj = csvjson.toObject(data, convertOptions);
+    
+  console.log('returng json response');
+  res.send(jsonobj);
+  
+});
+
+router.post('/genjsonfile', function(req, res, next) {
+  console.log('about to convert to json file');
+  
+  var data = fs.readFileSync('telemetry_sample.csv', {encoding: 'utf8'});
+  var convertOptions = { delimeter : ',' };
+  
+  var jsonobj = csvjson.toObject(data, convertOptions);
+  fs.writeFile("telemetry_out.json", util.inspect(jsonobj), function(err) {
+    if(err)
+      return console.log(err);
+  });
+  
+  console.log('wrote to json file');
+  res.status(204).end();
+  
+});
+
 
 module.exports = router;
